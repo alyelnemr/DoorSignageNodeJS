bb66e4df6088e21b2f28e213288975e33091c0fe
aly5elnemr@gmail.com <aly5elnemr@gmail.com>
2023-03-18 15:04:51 +0300

    add image empty path function

--- dboperation.js
+++ dboperation.js
@@ -2,31 +2,6 @@ var config = require("./dbconfig");
 const sql = require("mssql");
 const sql2 = require("msnodesqlv8");
  
-async function getdata() {
-  try {
-    let pool = await sql.connect(config.config_queue);
-    console.log("sql server connected...");
-  } catch (error) {
-console.log(" db-error :" + error);
-  }
-}
- 
- 
-async function getdata2(cliniID=1) {
-    var connStr = "Driver={SQL Server};user=mirth;password=mirth;Server=hjh-repsrv-03;Database=dbDoorSignage;Trusted_Connection=Yes;";
-    var query =
-    `SELECT       *
-    FROM            tblDoctorSchedule
-        WHERE ClinicID = ?`;
-    let x = await sql2.query(connStr, query, [cliniID], function(err, rows) {
-              if(err) console.log(err);
-              // console.log(rows);
-              // return JSON.stringify(rows);
-            });
-            console.log("x: " + x);
-    return x;
-}
- 
 
 async function getAllData() {
     try {
@@ -42,9 +17,9 @@ async function getAllData() {
  
 async function getDuration() {
     try {
-      var query = `SELECT RowID, ConfigName, ConfigValue, ConfigDescription
-FROM            tlbConfiguration
-WHERE        (ConfigName = 'Duration')`;
+      var query = `SELECT ConfigKey, ConfigValue
+FROM            tblConfiguration
+WHERE        (ConfigKey = 'Duration')`;
 console.log(" query :" + query);
       let pool = await sql.connect(config.config_queue);
       let res = await pool.request().query(query,);
@@ -59,10 +34,10 @@ async function getDataByClinicID(cliniID=1) {
       var query = `SELECT        
 	  ID, DoctorNameAR, DoctorNameEN, ClinicID, SpecialtyAR, SpecialtyEN, ClinicStartDate, ClinicEndDate, 
 	  'http://10.102.111.30:1020/api/getImageByID/' +cast(id as varchar) as ImagePath, ImageBinary
-      FROM            tblDoctorSchedule
-        WHERE ClinicID = @ClinicID`;
+      FROM            Room
+        WHERE ID = @RoomID`;
       let pool = await sql.connect(config.config_queue);
-      let res = await pool.request().input('ClinicID', sql.Int, cliniID).query(query,);
+      let res = await pool.request().input('RoomID', sql.Int, cliniID).query(query,);
       return res.recordsets[0][0];
     } catch (error) {
       console.log(" db-error :" + error);
@@ -88,11 +63,17 @@ async function getClinicByIPAddress(clinicIPAddress=1) {
 async function getClinicByForceIPAddress(clinicIPAddress=1) {
     try {
       var query = `
-	  select d.ID, DoctorNameAR, DoctorNameEN, ClinicID, ClinicNameAR, ClinicNameEN, SpecialtyAR, SpecialtyEN, ClinicStartDate, ClinicEndDate, 
-	  'http://10.102.111.30:1020/api/getImageByID/' +cast(d.id as varchar) as ImagePath
-      FROM            tblDoctorSchedule d inner join
-        tblClinic c on d.ClinicID = c.ID 
-  where [ClinicIPAddress] = @ClinicIPAddress`;
+      SELECT    Clinic.ID,    Clinic.ClinicName as ClinicNameEN, Clinic.ClinicName as ClinicNameAR, 
+      Physician.Name AS DoctorNameAR, Physician.NameEn AS DoctorNameEN, 
+      Clinic.Speciality, Clinic.DateTimeFrom, Clinic.DateTimeTo, Room.Name AS RoomName, 
+      Clinic.SpecialityEn as SpecialtyAR, Clinic.SpecialityEn as SpecialtyEN, 
+      Area.AreaName, Room.AreaID, Room.ComputerIPNumber, 'http://10.102.111.30:1020/api/getImageByID/' +cast(Room.ID as varchar) as ImagePath, 
+      'http://10.102.111.30:1020/api/getImageEmptyByID/' +cast(Room.ID as varchar) as ImagePathEmpty
+      FROM            Room INNER JOIN
+      Clinic ON Room.ID = Clinic.PlannedRoomID INNER JOIN
+      Physician ON Clinic.PhysicianID = Physician.ID AND Clinic.Active = 1 AND Clinic.StatusID = 2 INNER JOIN
+      Area ON Room.AreaID = Area.AreaID
+WHERE        (Room.AreaID = 2) AND (Room.ComputerIPNumber = @ClinicIPAddress)`;
       let pool = await sql.connect(config.config_queue);
       let res = await pool.request().input('clinicIPAddress', sql.VarChar, clinicIPAddress).query(query,);
       return res.recordsets[0][0];
@@ -103,12 +84,27 @@ async function getClinicByForceIPAddress(clinicIPAddress=1) {
 
 async function getImageByID(cliniID=1) {
     try {
-      var query = `SELECT     ImagePath
-        FROM            tblDoctorSchedule
-        WHERE ClinicID = @ClinicID`;
+      var query = `SELECT     Room.ImagePath
+        FROM            Room
+        WHERE ID = @RoomID`;
       let pool = await sql.connect(config.config_queue);
-      let res = await pool.request().input('ClinicID', sql.Int, cliniID).query(query,);
+      let res = await pool.request().input('RoomID', sql.Int, cliniID).query(query,);
       imgPath = res.recordset[0].ImagePath;
+      console.log(imgPath);
+      return imgPath;
+    } catch (error) {
+      console.log(" db-error :" + error);
+    }
+  }
+
+async function getImageEmptyByID(cliniID=1) {
+    try {
+      var query = `SELECT     Room.ImagePathEmpty
+        FROM            Room
+        WHERE ID = @RoomID`;
+      let pool = await sql.connect(config.config_queue);
+      let res = await pool.request().input('RoomID', sql.Int, cliniID).query(query,);
+      imgPath = res.recordset[0].ImagePathEmpty;
       console.log(imgPath);
       return imgPath;
     } catch (error) {
@@ -118,12 +114,12 @@ async function getImageByID(cliniID=1) {
   
 
 module.exports = {
-  getdata: getdata,
-  getdata2: getdata2,
+  getImageEmptyByID: getImageEmptyByID,
   getAllClinics: getAllData,
   getClinicByID: getDataByClinicID,
   getAllClinics: getAllData,
   getClinicByIPAddress: getClinicByIPAddress,
+  getClinicByForceIPAddress: getClinicByForceIPAddress,
   getImageByID: getImageByID,
   getDuration: getDuration
 };
--- routes/db.js
+++ routes/db.js
@@ -5,29 +5,8 @@ const sql = require("../dboperation");
 const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  sql.getdata();
-  res.render('db', { title: 'SQL Connection' });
-});
-
-/* GET home page. */
-router.get('/testold', function(req, res, next) {
-  sql.getdata();
-  res.render('db', { title: 'SQL Connection test' });
-});
-
-/* GET home page. */
 router.get('/getDuration', function(req, res, next) {
   var result = sql.getDuration().then((value) =>{
-    res.status(200).send(value);
-  });
-});
-
-/* GET home page. */
-router.get('/test22/:id', function(req, res, next) {
-  const { id } = req.params;
-  sql.getdata2(id).then((value) => {
-    // console.log(value);
     res.status(200).send(value);
   });
 });
@@ -67,7 +46,7 @@ router.get('/getClinicByForceIPAddress/:ipaddress', function(req, res, next) {
   var all_add = req.socket.remoteAddress.split(ipaddress);
   var add = all_add[all_add.length - 1];
   console.log(add);
-  var result = sql.getClinicByIPAddress(add).then((value) =>{
+  var result = sql.getClinicByForceIPAddress(add).then((value) =>{
     res.status(200).send(value);
   });
 });
@@ -76,7 +55,27 @@ router.get('/getClinicByForceIPAddress/:ipaddress', function(req, res, next) {
 router.get('/getImageByID/:id', function(req, res, next) {
   const { id } = req.params;
   var result = sql.getImageByID(id).then((value) =>{
-    imgPath = "\\\\HJH-REPSRV-03" + value;
+    imgPath = "\\\\hjh-queue-01\\door_signage_images\\" + value;
+    fs.access(imgPath, fs.constants.F_OK, err => {
+      console.log(`${imgPath} ${err ? "does not exist" : "exists"}`);
+    });
+    fs.readFile(imgPath, function(err, content) {
+      if (err) {
+        res.writeHead(404, { "Content-type" : "text/html" });
+        res.end(imgPath + "<h1> No Image </h1>");
+      } else {
+        res.writeHead(200, {"Content-type" : "image/png", 'aly-test': 'doct_name'});
+        res.end(content);
+      }
+    });
+  });
+});
+
+/* GET home page. */
+router.get('/getImageEmptyByID/:id', function(req, res, next) {
+  const { id } = req.params;
+  var result = sql.getImageEmptyByID(id).then((value) =>{
+    imgPath = "\\\\hjh-queue-01\\door_signage_images\\" + value;
     fs.access(imgPath, fs.constants.F_OK, err => {
       console.log(`${imgPath} ${err ? "does not exist" : "exists"}`);
     });
