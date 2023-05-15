a4c74b4ddc86846e0f9477c5eb8e99b42600c6e5
aly5elnemr@gmail.com <aly5elnemr@gmail.com>
2023-05-15 12:03:37 +0300

    LIVE

--- routes/db.js
+++ routes/db.js
@@ -12,50 +12,6 @@ router.get('/getDuration', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/RoomSetup/', function(req, res, next) {
-  var arr_rooms = [];
-  var title = 'IPD Door Signage Setup';
-
-  var result = sql.getAllIPDRooms().then((value) => {
-    res.render('index', { title: title, arr_rooms: value });
-    // res.status(200).send(value);
-  });
-});
-
-/* GET home page. */
-router.post('/SaveRoomSetup/', function(req, res, next) {
-  var arr_rooms = [];
-  var title = 'IPD Door Signage Setup';
-  var result = sql.updateIPDRoomByID(roomID= req.body.rooms, Image1=req.body.IsIPDImage1, 
-    Image2=req.body.IsIPDImage2, Image3=req.body.IsIPDImage3, Image4=req.body.IsIPDImage4, 
-    Image5=req.body.IsIPDImage5, Image6=req.body.IsIPDImage6, Image7=req.body.IsIPDImage7, 
-    Image8=req.body.IsIPDImage8).then(()=>{
-      res.redirect('/api/RoomSetup/');
-    });
-});
-
-/* GET home page. */
-router.get('/GetRoomSetup/:id?', function(req, res, next) {
-  const { id } = req.params;
-  var arr_rooms = [];
-  var title = 'IPD Door Signage Setup';
-  var roomID = 0;
-
-  if(!id && id != '' && id != 0) {
-  }else {
-    roomID = id;
-  }
-  if(!id && id != '' && id != 0) {
-  }else {
-    roomID = id;
-  }
-
-  var result = sql.getIPDRoomByID(roomID).then((value) => {
-    res.status(200).send(value[0]);
-  });
-});
-
-/* GET home page. */
 router.get('/getIP', function(req, res, next) {
   const { ipaddress } = req.params;
   var all_add = req.socket.remoteAddress.split(':');
